e1049501aecf0ea735bfd674313d1e3deafc0e0d
chiiiho <163135461+Chiiiho@users.noreply.github.com>
2024-06-02 11:17:47 +0900

    MySQLテーブルからidを削除、country_codeをPRIMARY KEYへ変更

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,13 +1,12 @@
 DROP TABLE IF EXISTS countries;
 
 CREATE TABLE countries (
- id int unsigned AUTO_INCREMENT,
+ country_code int NOT NULL,
  country VARCHAR(20) NOT NULL,
- country_code VARCHAR(20) NOT NULL,
  city VARCHAR(20) NOT NULL,
- PRIMARY KEY(id)
+ PRIMARY KEY(country_code)
 );
 
-INSERT INTO countries (country, country_code, city) VALUES ('United Kingdom', '44', 'London');
-INSERT INTO countries (country, country_code, city) VALUES ('France', '33', 'Paris');
-INSERT INTO countries (country, country_code, city) VALUES ('Germany', '49', 'Berlin');
+INSERT INTO countries (country_code, country, city) VALUES (44, 'United Kingdom', 'London');
+INSERT INTO countries (country_code, country, city) VALUES (33, 'France', 'Paris');
+INSERT INTO countries (country_code, country, city) VALUES (49, 'Germany', 'Berlin');
